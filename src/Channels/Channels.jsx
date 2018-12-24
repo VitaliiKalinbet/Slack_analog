9041179ba6473de57af95e896614436a632cd8d1
VitaliiKalinbet <bosssvit@gmail.com>
2018-12-24 12:26:11 +0200

    fetch_channel_from_firebase done

--- src/Channels/Channels.jsx
+++ src/Channels/Channels.jsx
@@ -14,6 +14,21 @@ class Channels extends Component {
         channelsRef: firebase.database().ref('channels'),
     }
 
+    componentDidMount () {
+        this.addListener();
+    }
+
+    addListener = () => {
+        let loadedChannels = [];
+        this.state.channelsRef.on('child_added', snap => {
+            loadedChannels.push(snap.val())
+            console.log(loadedChannels);
+            this.setState({
+                channels: loadedChannels,
+            })
+        })
+    }
+
     openModal = () => {
         this.setState({
             modal: true,
@@ -79,6 +94,15 @@ class Channels extends Component {
                     <Icon name='exchange'/> CHANNELS
                 </span> ({channels.length})<Icon name='add' onClick={this.openModal}/>
             </Menu.Item>
+            {channels.length > 0 && channels.map(channel => {
+                return <Menu.Item
+                    key={channel.id}
+                    name={channel.name}
+                    style={{opacity:0.7}}
+                    >
+                # {channel.name}    
+                </Menu.Item>
+            })}
             </Menu.Menu>
             <Modal open={modal} onClose={this.closeModal} style={{background:'#fff'}}>
             <Modal.Header>Add a Channel</Modal.Header>
