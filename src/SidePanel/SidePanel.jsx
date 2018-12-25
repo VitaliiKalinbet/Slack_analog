5ad4af78afcacb6df90258366a5cb56a9d94080a
VitaliiKalinbet <bosssvit@gmail.com>
2018-12-25 15:23:34 +0200

    direct_message done

--- src/SidePanel/SidePanel.jsx
+++ src/SidePanel/SidePanel.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from '../UserPanel/UserPanel';
 import Channels from '../Channels/Channels';
+import DirectMessage from '../DirectMessage/DirectMessage';
 
 class SidePanel extends Component {
     render() {
@@ -14,6 +15,7 @@ class SidePanel extends Component {
             style={{background: '#4c3c4c', fontSize:'1.2rem'}}>
                 <UserPanel/>
                 <Channels/>
+                <DirectMessage/>
             </Menu>
         );
     }
